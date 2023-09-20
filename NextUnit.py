2549bd9e3e0462638547464ee612ac75fadbade9
EyadAbdelfatah <144480862+EyadAbdelfatah@users.noreply.github.com>
2023-09-20 14:01:06 -0400

    odd or even

--- NextUnit.py
+++ NextUnit.py
@@ -5,10 +5,25 @@ print(x,y)
 x = "test"
 print(f"hello {x}")
 
-temp = 75
+temp = float(68)
 if temp > 68:
     print('warm')
 elif temp == 68:
     print('perfect')
 else:
     print('cold')
+
+values = [1,2,23,5,7,2,30,15]
+print(values[2])
+
+x = "this is a thing"
+y= x.split( )
+z = y[0]
+print(y)
+print(z)
+
+day_of_week = input("what day is it? ")
+if day_of_week == "Friday":
+    print("correct")
+else:
+    print("incorrect")
---
+++ challenge.py
@@ -0,0 +1,10 @@
+x=int(input("Enter a number:"))
+e=("even")  
+o= ("odd")
+def oddoreven(x):
+    print({x})
+    if x % 2 == 0:
+        print(e)
+    else: 
+        print(o)
+oddoreven(x)
