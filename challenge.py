e78b2cc9ffa89abe84d0b8878adf6be5e83ef277
EyadAbdelfatah <144480862+EyadAbdelfatah@users.noreply.github.com>
2023-09-21 14:01:08 -0400

    Update challenge.py

--- challenge.py
+++ challenge.py
@@ -7,4 +7,22 @@ def oddoreven(x):
         print(e)
     else: 
         print(o)
-oddoreven(x)
+
+
+g=("good")
+b=("bad")
+o=("okay")
+gr=("great")
+y=int(input("What was your bill?:"))
+x=(input("How was your service?:"))
+def tips(x):
+    print({x})
+    if {g}:
+        print(1.2* y)
+    elif {b}:
+        print(y)
+    elif {o}:
+        print(1.15*y)
+    else :
+        print(1.25*y)
+tips(x)
