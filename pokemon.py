93a6f7fe6f79aa75c29ba7fb7e0b029f20289007
EyadAbdelfatah <144480862+EyadAbdelfatah@users.noreply.github.com>
2023-10-03 13:34:23 -0400

    Create pokemon.py

---
+++ pokemon.py
@@ -0,0 +1,17 @@
+#You are running away from an angry pokemon. Luckily for you, you have a great ball and there's an ivysaur, a blastoise, and a charizard hiding in the bushes. 
+#Unfortunately, it's very late at night and you can't tell which is which. If you capture the right pokemon you'll live. If you don't capture the right pokemon or 
+# if the great ball doesnt capture, you die.
+
+def pokemon(x,y,z):
+    print("You threw your great ball at a one of the pokemon!")
+    if z>=50:
+        print("You successfully captured a pokemon")
+    else:
+        print("Your great ball broke and you are now dead!")
+    if (x=Charizard and y=Blastoise and z>=50):
+        print("You encountered a charizard and caught a blastoise. Fortunately, the charizard was not very effective against the blastoise and killed it. You are now alive.")
+    if ():
+    if (x=Blastoise and y=Ivysaur and z>=50):
+        print("You encountered a blastoise and caught a ivysaur. Fortunately, the ivysaur was super effective against the blastoise and killed it. You are now alive.")
+    if (x=):
+pokemon(Charizard,Blastoise,50)
